5e9256f2597fd371c62bcbf2b84c116bd19ee173
Vlad <pacom4n90@gmail.com>
2025-09-24 18:33:58 +0000

    Primer intento JWT

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,8 +5,53 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+import bcrypt
+
 
 api = Blueprint('api', __name__)
+
+@api.route('/register', methods=['POST'])
+def register():
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+
+    if User.query.filter_by(email=email).first():
+        return jsonify({"msg": "El usuario EXISTE"}), 400
+
+    
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+
+
+    user = User(email=email, password=hashed_password.decode('utf-8'), is_active=True)
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify(user.serialize()), 201
+
+
+@api.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+
+    user = User.query.filter_by(email=email).first()
+    if not user:
+        return jsonify({"msg": "Credenciales inválidas"}), 401
+
+
+    if not bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+        return jsonify({"msg": "Credenciales inválidas"}), 401
+
+    token = create_access_token(identity=user.id)
+    return jsonify({"token": token, "user": user.serialize()}), 200
+
+
+
+
 
 # Allow CORS requests to this API
 CORS(api)
